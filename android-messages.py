bc49f123db310eb55cb1a30f1642f97e1e5ede3d
Ian Markind <ianmarkind@gmail.com>
2018-08-29 21:28:14 -0400

    created android messages script and removed caps lock from bitbucket.

---
+++ android-messages.py
@@ -0,0 +1,21 @@
+from talon.voice import Context, Key
+
+ctx = Context('android-messages', func=lambda app, win: 'Messages for web' in win.title)
+
+keymap = {
+
+    'new conversation': Key('cmd-alt-c'),
+    'next conversation': Key('ctrl-.'),
+    'previous conversation': Key('ctrl-,'),
+    'delete conversation': Key('cmd-alt-r'),
+    'archive conversation': Key('cmd-alt-h'),
+    'open settings': Key('cmd-alt-x'),
+    'attach files': Key('cmd-alt-a'),
+    '(show | hide) emoji picker': Key('cmd-alt-e'),
+    '(show | hide) sticker picker': Key('cmd-alt-s'),
+    '(show | hide) gif picker': Key('cmd-alt-g'),
+    '(show | hide) details': Key('cmd-alt-d'),
+
+}
+
+ctx.keymap(keymap)
